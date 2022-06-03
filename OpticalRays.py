2f797d131330cf079a27221ddfef2d5f2c3a2cf7
Isobel Bicket <bicketic@mcmaster.ca>
2022-06-03 14:47:24 -0400

    Initializing repository with some initial classes for rays and a thin lens

---
+++ OpticalRays.py
@@ -0,0 +1,44 @@
+class OpticalRay:
+    def __init__(self, origin_point, origin_angle):
+        self.angles = [origin_angle,] # a single angle (radians)
+        #self.angles = np.append(self.angles, origin_angle)
+        
+        # to contain m and b from y=mx+b for each path the ray travels
+        initial_path_slope, initial_path_intercept = self.calculate_linear_equation(-origin_angle, origin_point)
+        self.paths = np.array([[initial_path_slope, initial_path_intercept]])
+        
+        self.path_vertices = np.expand_dims(origin_point, axis=0)
+        self.add_placeholder_vertex(origin_point[0] + 10)
+        self.lenses = []
+    
+    def calculate_linear_equation(self, angle, origin):
+        # angle: radians
+        # origin: (x,y) coordinate pair
+        # y=mx+b
+        intercept = origin[0] * np.tan(angle) + origin[1]
+        slope = np.tan(angle)
+        return slope, intercept
+    
+    def add_lens(self, new_lens):
+        # lens_location: x value where the next lens sits
+        # account for lenses being added at x-values less than previous lens TODO
+        incident_y = self.paths[-1, 0] * new_lens.location + self.paths[-1, 1]
+        outgoing_angle = calculate_outgoing_angle(self.angles[-1], incident_y, new_lens.f)
+        outgoing_slope, outgoing_intercept = self.calculate_linear_equation(
+            -outgoing_angle, [new_lens.location, incident_y]
+        )
+        self.lenses.append(new_lens)
+        self.angles.append(outgoing_angle)
+        self.path_vertices[-1, :] = np.array([[new_lens.location, incident_y]])
+        self.paths = np.append(self.paths, np.array([[outgoing_slope, outgoing_intercept]]), axis=0)
+        self.add_placeholder_vertex(new_lens.location+10)
+        return
+    
+    def add_placeholder_vertex(self, x_location):
+        # for seeing the ray during plotting
+        self.path_vertices = np.append(
+            self.path_vertices,
+            np.array([[100, self.paths[-1, 0]*100+self.paths[-1, 1]]]),
+            axis=0
+        )
+
---
+++ ThinLens.py
@@ -0,0 +1,4 @@
+class ThinLens:
+    def __init__(self, f, location):
+        self.f = f # focal distance
+        self.location = location # x-location of the lens
